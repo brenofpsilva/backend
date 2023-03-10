b49932c4fa48ddfc210bdee38bb365225a860a74
brenosilva <brenofpsilva@hotmail.com>
2023-03-10 19:14:10 -0300

    Alter migrate employees add status

--- database/migrations/2023_03_10_200311_create_employees_table.php
+++ database/migrations/2023_03_10_200311_create_employees_table.php
@@ -14,10 +14,11 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('cpf');
-            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('phone')->nullable();
             $table->string('knowledge');
+            $table->enum('status', ['validated','not_validated']);
             $table->timestamps();
         });
     }
